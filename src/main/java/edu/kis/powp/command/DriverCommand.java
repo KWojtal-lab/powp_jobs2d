063ee1d185fb9d41fdecb1b228f1d8bec3405328
Krzysztof <258380@edu.p.lodz.pl>
2026-03-25 13:04:20 +0100

    job2d setter in command

--- src/main/java/edu/kis/powp/command/DriverCommand.java
+++ src/main/java/edu/kis/powp/command/DriverCommand.java
@@ -5,4 +5,8 @@ import edu.kis.powp.jobs2d.Job2dDriver;
 public abstract class DriverCommand {
     protected Job2dDriver driver;
     abstract void execute();
+
+    public void setDriver(Job2dDriver driver) {
+        this.driver = driver;
+    }
 }
